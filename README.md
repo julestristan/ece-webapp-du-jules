ba285b22ad41bed92bad40e53151ae1b05199a3a
Thomas Du <thomas.du@edu.ece.fr>
2022-10-12 00:25:43 +0200

    feat: lab3 part 1 and readme

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 Repository du projet Webtech 2022
 
+## Essayer le code
+
+Commencez par initialiser npm : 
+```bash
+    npm init -y
+```
+
+Puis :
+```bash
+    npm run develop
+```
+
+Accedez à la page d'accueil via : http://localhost:3000/
+
+Voici les routes disponibles :
+- http://localhost:3000/hello/ + *nom*
+- http://localhost:3000/about
+
 ## Contributeurs
 - Tristan JULES
-- Thomas :DU
+- Thomas DU
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const handles = require('./handles')
 const express = require('express')
+const myAbout = require('./content/about.json')
 
 const app = express()
 const port = 3000
@@ -8,16 +9,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/user', (req, res) => {
-  res.send(req.params)
-})
+app.get(
+  '/hello',
+  (req, res) => {
+    res.send('Hello anonymous\n')
+  }
+)
 
-app.put('/about', (req, res) => {
-  res.send('Got a PUT request at /about')
-})
+app.get(
+  '/hello/:name', 
+  function (req, res) {
+    if(req.params.name === 'Thomas') { // http://localhost:3000/hello?name=Thomas
+      res.send('I\'m ' + req.params.name + ' I\'m a student\n')
+    }
+    else {
+      res.send('Hello ' + req.params.name + '\n') // params['name'] == params.name
+    }
+  }
+)
+
+app.get(
+  '/about',
+  (req, res) => {
+    res.send(JSON.stringify(myAbout) + '\n')
+  }
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// http.createServer(handles.serverHandle).listen(port, () => console.log(`Server is running at localhost: ${port}`))
