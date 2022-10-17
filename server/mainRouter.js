0a6cdaf76479dfef51f566d5bd532a26f56016c8
Thomas Du <thomas.du@edu.ece.fr>
2022-10-17 19:34:32 +0200

    feat: get all comments of the article with articleId

--- server/mainRouter.js
+++ server/mainRouter.js
@@ -58,14 +58,29 @@ let db = {
       articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
       author: 'Bob McLaren'
     },
+    {
+      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+      timestamp: 1664835049,
+      content: 'Content of the comment.',
+      articleId: '1',
+      author: 'Bob McLaren'
+    },
+    {
+      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+      timestamp: 1664835049,
+      content: 'Content of the comment.',
+      articleId: '1',
+      author: 'Thomas'
+    },
     // ...
   ]
 }
   
 router.get('/articles', (req, res) => {
   res.writeHead(200, {'Content-Type' : 'text/html'})
-  db.articles.forEach(function(item, index, array) {
-    displayArticle(item, res)
+  res.write(`<h1>List of the articles:<br></h1>`)
+  db.articles.forEach(function(item, index) {
+    displayArticleInList(item, index, res)
   })
   res.end()
 })
@@ -84,7 +99,6 @@ router.post('/articles', (req, res) => {
 router.get('/articles/:articleId', (req, res) => {
   res.writeHead(200, {'Content-Type' : 'text/html'})
   const targetArticle = db.articles.find(article => article.id == req.params.articleId)
-  console.log(targetArticle)
   if(targetArticle)
     displayArticle(targetArticle, res)
   else
@@ -92,10 +106,39 @@ router.get('/articles/:articleId', (req, res) => {
   res.end()
 })
 
+router.get('/articles/:articleId/comments', (req, res) => {
+  res.writeHead(200, {'Content-Type' : 'text/html'})
+  const targetArticle = db.articles.find(article => article.id == req.params.articleId)
+  if(targetArticle){
+
+    res.write(`<h1>Article with ID : ${req.params.articleId}</h1>`)
+    displayArticle(targetArticle, res)
+
+    res.write(`<h1>Comments:</h1>`)
+    db.comments.forEach(function(comment) {
+      if(comment.articleId == targetArticle.id)
+        displayComment(comment, res)
+    })
+  }
+  else
+    res.write(`Cannot find article with id : ${req.params.articleId}`)
+  res.end()
+})
+
+function displayArticleInList(article, index, res) {
+  res.write(`<h2>Article ${index + 1}</h2>`)
+  displayArticle(article, res)
+}
+
 function displayArticle(article, res) {
   res.write(article.title + '<br>\n')
   res.write(article.content + '<br>\n')
   res.write('<br>\n')
 }
 
+function displayComment(comment, res) {
+  res.write(`<h3>${comment.author}</h3>`)
+  res.write(comment.content + '<br>\n')
+  res.write('<br>\n')
+}
 module.exports = router
